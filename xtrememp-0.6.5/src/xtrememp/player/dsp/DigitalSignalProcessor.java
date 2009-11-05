42130646d52286f660949bbb3f13ee0d83632fbb
besidj <besidj@5c1a2c8a-5c6e-4d4c-80b9-fe86a6a460a1>
2009-11-05 20:06:58 +0000

    bugfixes, improvements and other changes...
    git-svn-id: https://xtrememp.svn.sourceforge.net/svnroot/xtrememp@23 5c1a2c8a-5c6e-4d4c-80b9-fe86a6a460a1

---
+++ xtrememp-0.6.5/src/xtrememp/player/dsp/DigitalSignalProcessor.java
@@ -0,0 +1,45 @@
+/**
+ * Xtreme Media Player a cross-platform media player.
+ * Copyright (C) 2005-2009 Besmir Beqiri
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package xtrememp.player.dsp;
+
+import javax.sound.sampled.SourceDataLine;
+
+/**
+ * @author Besmir Beqiri
+ */
+public interface DigitalSignalProcessor {
+
+    /**
+     * Called by the DigitalSignalSynchronizer during the call of the 'start' method.
+     * Allows a DSP to prepare any necessary buffers or object according to the audio format of
+     * the source data line.
+     *
+     * @param sampleSize     The sample size that this DSP should be prepared to handle.
+     * @param sourceDataLine The source data line that will be monitored.
+     */
+    void initialize(int sampleSize, SourceDataLine sourceDataLine);
+
+    /**
+     * Called by the DigitalSignalSynchronizer while the SourceDataLine is active.
+     *
+     * @param dssContext A context object containing a reference to the sample data to be processed
+     * 					  as well as other useful references during processing time.
+     */
+    void process(DigitalSignalSynchronizer.Context dssContext);
+}
